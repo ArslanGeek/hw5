5841be52acf66e4a71087d260e75bc238d78eac1
ArslanGeek <bra.kreedz@gmail.com>
2023-07-19 15:57:38 +0600

    sps

---
+++ pythonProject/dop_lesson.py
@@ -0,0 +1,34 @@
+# ctrl + alt + l = выравнивает код
+# ctrl + shift + up/down = двигает строку вверх\вниз
+# alt = одновременный набор в нескольких строках
+
+
+
+# svetofor = input('Цвет светофора? ').lower()
+#
+# if svetofor == 'green':
+#     print(f'Go!')
+# elif svetofor == 'yellow':
+#     print(f'Wait!')
+# elif svetofor == 'red':
+#     print(f'Stop!')
+# else:
+#     print(f'Error!')
+
+
+#
+# while True:
+#     print('Hello')
+
+# print('qwerty'.index())
+
+
+# print[i*i for i in range(1, 6)]
+
+
+
+
+
+
+
+
